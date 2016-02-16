15f6fa14efb0adc69a035e51b84f497023ab6e38
MooGu Z <moogu@live.cn>
2016-02-16 13:17:46 -0500

    add support to save and restore frame settings automatically

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -441,13 +441,40 @@ and file 'filename' will be opened and cursor set on line 'linenumber'"
   (assq 'geometry (car (display-monitor-attributes-list))))
 (defconst monitor-width (nth 3 monitor-geoinfo))
 (defconst monitor-height (nth 4 monitor-geoinfo))
-;; set frame font
-(if (> monitor-width 2000)
-    (set-frame-font "Menlo 15")
-  (set-frame-font "Menlo 13"))
-;; set frame size
-(setq default-frame-alist
-      '((width . 87) (height . 47)))
+;; set variables and functions to save and restore frame info
+(defvar frame-width-record 87)
+(defvar frame-height-record 47)
+;; alternative fonts :
+;; 1. Source Code Pro
+;; 2. Menlo
+;; 3. Courier
+;; 4. Andale Mono
+;; 5. Monaco
+;; 6. Consolas
+;; 7. Dejavu Sans Mono (default)
+(defvar frame-font-record
+  (if (> monitor-width 1920)
+      "Dejavu Sans Mono 15"
+    "Dejavu Sans Mono 13"))
+(defun save-frame-setting (&optional f)
+  "Save current frame (F) info into pre-defined variables."
+  (setq frame-width-record (frame-width))
+  (setq frame-height-record (frame-height))
+  (setq frame-font-record
+        (frame-parameter (selected-frame) 'font)))
+(defun restore-frame-setting (f)
+  "Restore frame setting from records to current frame F."
+  (set-frame-size f frame-width-record frame-height-record nil)
+  (set-frame-font frame-font-record nil (list f)))
+;; save frame info before frame closed
+(add-hook 'delete-frame-functions 'save-frame-setting)
+(add-hook 'after-make-frame-functions 'restore-frame-setting)
+;; set frame font and size
+;; (set-frame-font frame-font-record)
+;; (setq default-frame-alist
+;;       '((width  . frame-width-record)
+;;         (height . frame-height-record)))
+;; (set-frame-font frame-font-record)
 
 ;; Edit as Root User
 (defun sudo-edit (&optional arg)
@@ -464,6 +491,9 @@ Without ARG interactive prompt would appear."
       (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name)))))
 (global-set-key (kbd "C-c C-w") 'sudo-edit)
 
+;; set up shell environment
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
 
 (provide 'prelude-editor)
 
