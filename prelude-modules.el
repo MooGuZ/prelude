9e1a27f91bcc814996b108de65156e2e68e42545
MooGu Z <moogu@live.cn>
2016-02-16 01:30:12 -0500

    modified '.gitignore' to include 'prelude-modules.el'

---
+++ prelude-modules.el
@@ -0,0 +1,24 @@
+;;; prelude-module --- initialization of modules
+
+;;; Commentary:
+
+;; Configuration file controling loading of modules
+
+;;; Code:
+
+(require 'prelude-ido)
+(require 'prelude-org)
+
+(require 'prelude-emacs-lisp)
+
+(require 'prelude-c)
+(require 'prelude-js)
+(require 'prelude-lisp)
+(require 'prelude-python)
+(require 'prelude-matlab)
+
+(require 'prelude-xml)
+
+(require 'prelude-latex)
+
+;; END
