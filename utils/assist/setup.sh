e55a913f899b81d8536e17dfa49caa9bf147ed71
MooGuZ <moogu@live.cn>
2016-04-19 12:34:09 -0400

    some updates
    1. modify Info.plist of EmacsLauncher.app to let it registers for
       multiple file types.
    2. move EmacsLauncher.app to User's Application directory
    3. fixed the problem of open file with path containing white space,
       however, the solution disable the program to open multiple files
       at once.

--- utils/assist/setup.sh
+++ utils/assist/setup.sh
@@ -3,7 +3,7 @@ CURDIR=$(dirname $0)
 # setup EDIT command
 cp $CURDIR/edit /usr/local/bin/
 # setup EmacsLauncher
-cp -R $CURDIR/EmacsLauncher.app /Applications/
+cp -R $CURDIR/EmacsLauncher.app ~/Applications/
 # setup logout hook
 sudo defaults write com.apple.loginwindow LogoutHook $CURDIR/killEmacsBeforeLogout.sh
 # END
